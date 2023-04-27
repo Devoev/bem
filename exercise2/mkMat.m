dfed740e6c6688b265e41193ee289ea0a226ee84
devin <devinbalian@gmail.com>
2023-04-27 10:07:19 +0200

    exercise 2 init

---
+++ exercise2/mkMat.m
@@ -0,0 +1,8 @@
+function sysmat = mkMat(Geom)
+%
+%	Assembles the Matrix. Gets the geometry as input and uses piecewise constant functions.
+%
+% #############################################################################
+% # Task 3c)
+% #############################################################################
+end
