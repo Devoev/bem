f7086f2b98f3853ba68039487870b7094fd06193
Devin <devinbalian@gmail.com>
2023-04-27 17:23:56 +0200

    ex2 task 3 b)

--- exercise2/mkMat.m
+++ exercise2/mkMat.m
@@ -5,4 +5,18 @@ function sysmat = mkMat(Geom)
 % #############################################################################
 % # Task 3c)
 % #############################################################################
+
+    [n,d] = size(Geom);
+    n = n-1;
+    sysmat = zeros(n,n);
+
+    for i = 1:n
+        for j = 1:n
+            xi = Geom(i,1);
+            yj = Geom(j,2);
+            yj2 = Geom(j+1,2);
+            sysmat(i,j) = quadgk(@(y) fundamentalsol(xi,y), yj, yj2); % fixme: Change integration path
+        end
+    end
+
 end
