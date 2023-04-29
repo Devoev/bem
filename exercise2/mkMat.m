885c0d3ba0f603b143a69df3cec7143660226d74
Devin <devinbalian@gmail.com>
2023-04-29 14:39:49 +0200

    Implemented analytical solution of integral

--- exercise2/mkMat.m
+++ exercise2/mkMat.m
@@ -13,9 +13,12 @@ function sysmat = mkMat(Geom)
     for i = 1:n
         for j = 1:n
             xi = Geom(i,1);
-            yj = Geom(j,2);
-            yj2 = Geom(j+1,2);
-            sysmat(i,j) = quadgk(@(y) fundamentalsol(xi,y), yj, yj2); % fixme: Change integration path
+            y1 = Geom(j,2);
+            y2 = Geom(j+1,2);
+            %sysmat(i,j) = quadgk(@(y) fundamentalsol(xi,y), yj, yj2); % fixme: Change integration path
+            u0 = xi - y1;
+            u1 = xi - y2;
+            sysmat(i,j) = (u1*(log(abs(u1)) - 1) - u0*(log(abs(u0)) - 1)) / (-2*pi);
         end
     end
 
