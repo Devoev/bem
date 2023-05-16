f20d8e328d0b77d908c1afb0b1ce960fbf980584
Devin <devinbalian@gmail.com>
2023-05-16 15:45:07 +0200

    exercise 3 init

---
+++ exercise3/data.m
@@ -0,0 +1,8 @@
+function out = data(ptX,ptY,c)
+	% This function is used to generate the right hand side
+	% #############################################################################
+	% # Task 3b)
+	% #############################################################################
+	assert("This should be implemented!");
+  out = 0;
+end
