9e11c16d3d4cd47617751d324331e58a4c97c629
Devin <devinbalian@gmail.com>
2023-05-24 21:56:41 +0200

    fixed error in poteval.m

--- exercise3/poteval.m
+++ exercise3/poteval.m
@@ -11,8 +11,8 @@ for i = 1:nx
         for k = 1:n
         	% Parametrizes the element wrt. x in [0,1], then calculates the fundamental solution
             f = @(x) fundamentalsol((x)*Geom(k,:)+(1-x)*Geom(k+1,:),[ptX(i,j),ptY(i,j)]);
-            val(i,j) = val(i,j) +X(k)*quad(f,0,1)*norm(Geom(k,:)-Geom(k+1,:),2); % For octave, Adds the value to the sum
-            % val(i,j) = val(i,j) +X(k)*integral(f,0,1,'ArrayValued',1)*norm(Geom(k,:)-Geom(k+1,:),2); % For matlab, Adds the value to the sum
+%            val(i,j) = val(i,j) +X(k)*quad(f,0,1)*norm(Geom(k,:)-Geom(k+1,:),2); % For octave, Adds the value to the sum
+             val(i,j) = val(i,j) +X(k)*integral(f,0,1,'ArrayValued',1)*norm(Geom(k,:)-Geom(k+1,:),2); % For matlab, Adds the value to the sum
         end
     end
 end
